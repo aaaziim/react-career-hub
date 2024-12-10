0ab1dc7e8896454b98dc062c9587cbb289295b9c
aaaziim <azim210215@gmail.com>
2024-12-11 03:43:48 +0600

    Banner Added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,21 @@
-
+import model from "../../assets/images/user.png"
 const Banner = () => {
     return (
-        <div className="h-44 flex justify-center items-center bg-slate-300 my-10">
-            <h2>HomePage Banner</h2>
+        <div >
+            <div className="hero bg-base-200 h-1/3">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img
+                        src={model}
+                        className="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 className="text-5xl font-bold">One Step Closer</h1>
+                        <p className="text-3xl py-6">
+                            to get your dream job.
+                        </p>
+                        <button className="btn btn-primary">Apply Now!</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
